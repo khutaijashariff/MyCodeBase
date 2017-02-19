3cb92526d55c5f501e48e78816e7ee63177118ab
Anjali Selvaraj <anjali_selvaraj@yahoo.in>
2017-02-18 21:27:38 -0800

    Create BubbleSort.java

---
+++ Sorts/BubbleSort.java
@@ -0,0 +1,21 @@
+package Sorts;
+
+import javaservices.Swap;
+
+/**
+ * Created by ANJALI on 01/30/17.
+ */
+class BubbleSort {
+    static void bSort(int arr[])
+    {
+        for(int i=0;i<=arr.length;i++) //picks an element
+        {
+            for(int j=1;j<arr.length-i;j++) //always starts with 1 and goes till arr.len -i bcos the last i elements would be in the right place
+            {
+                if(arr[j-1]>arr[j]) // compares every element with the previous elemnt
+                                      // pushes bigger number to the end of the array
+                    Swap.swapArray(arr, j - 1, j);
+            }
+        }
+    }
+}
