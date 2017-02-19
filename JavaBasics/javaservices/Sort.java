1d49a216ac4bd6e494124258c7d2baab4ec12491
Anjali Selvaraj <anjali_selvaraj@yahoo.in>
2017-02-18 21:39:03 -0800

    General Sorting

---
+++ JavaBasics/javaservices/Sort.java
@@ -0,0 +1,14 @@
+package javaservices;
+
+/**
+ * Created by ANJALI on 2/9/2017.
+ */
+public class Sort {
+    public static void sort(int[] arr)
+    {
+        for(int i=0;i<arr.length;i++)
+            for(int j=0;j<arr.length;j++)
+                if(arr[i]<arr[j])
+                    Swap.swapArray(arr,i,j); //since both Sort and Swap are in the same package, import is not required
+    }
+}
