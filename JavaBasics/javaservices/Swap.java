ac77933fbff6d1054835a6a04c5953416bb2399b
Anjali Selvaraj <anjali_selvaraj@yahoo.in>
2017-02-18 21:39:53 -0800

    Swapping

---
+++ JavaBasics/javaservices/Swap.java
@@ -0,0 +1,13 @@
+package javaservices;
+
+/**
+ * Created by ANJALI on 01/30/17.
+ */
+public class Swap {
+   public static void swapArray(int[] arr,int index1,int index2)
+    {
+        int temp=arr[index1];
+        arr[index1]=arr[index2];
+        arr[index2]=temp;
+    }
+}
