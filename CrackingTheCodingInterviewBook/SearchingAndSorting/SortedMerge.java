c0fabc2ec87f3bfa23c01057b19e863e50f2e7a4
Anjali Selvaraj <anjali_selvaraj@yahoo.in>
2017-02-28 16:33:53 -0800

    update SortedMerge

--- CrackingTheCodingInterviewBook/SearchingAndSorting/SortedMerge.java
+++ CrackingTheCodingInterviewBook/SearchingAndSorting/SortedMerge.java
@@ -1,5 +1,7 @@
 package CrackingTheCodingInterviewBook.SearchingAndSorting;
 
+import java.util.Scanner;
+
 /**
  * Created by ANJALI on 2/26/2017.
  */
@@ -9,6 +11,9 @@ Write a method to merge B into A in sorted order.
  */
 
 class SortedMerge {
+   private int[] arrA,arrB;
+   private int sizeA,sizeB;
+    /* USES SIMPLE MERGE AND SORT
     int[] sortedMerge(int[] arrA,int[] arrB)
     {
         int merged=arrA.length-1;
@@ -19,14 +24,54 @@ class SortedMerge {
         }
         Algos.Sorts.QuickSort.qSort(arrA,0,arrA.length-1);
        return arrA;
+    }*/
+    private void inputForSortedMerge()
+    {
+        Scanner s= new Scanner(System.in);
+        System.out.println("****Sorted Merge***");
+        System.out.println("Enter the number of elements in A");
+        sizeA=s.nextInt();
+        System.out.println("Enter the number of elements in B");
+        sizeB=s.nextInt();
+        arrA=new int[sizeA+sizeB];
+        arrB=new int[sizeB];
+        System.out.println("Enter the elements for A");
+        for(int i=0;i<sizeA;i++)
+            arrA[i]=s.nextInt();
+        System.out.println("Enter the elements for B");
+        for(int i=0;i<sizeB;i++)
+            arrB[i]=s.nextInt();
     }
+   private int[] sortedMerge()
+    {
+       int indexA=sizeA-1;
+       int indexB=sizeB-1;
+       int mergedIndex=(sizeA+sizeB)-1;
+
+       while(indexB>=0) //Since arrA is already sorted, the only condition required is to insert arrB elements in order
+       {
+          if(indexA>=0 && arrA[indexA]>arrB[indexB] )
+           {
+               arrA[mergedIndex]=arrA[indexA];
+               indexA--;
+           }
+           else
+           {
+               arrA[mergedIndex]=arrB[indexB];
+               indexB--;
+           }
+           mergedIndex--;
+       }
+
+
+     return arrA;
+    }
+
     public static void main(String args[])
     {
-        int[] arrA={3,9,10,Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE};
-        int[] arrB={1,4,5};
         SortedMerge sm=new SortedMerge();
-        sm.sortedMerge(arrA,arrB);
-        for(int i:arrA)
-            System.out.print(i +"\t");
+        sm.inputForSortedMerge();
+        for(int i:sm.sortedMerge())
+            System.out.print(i+"\t");
     }
 }
